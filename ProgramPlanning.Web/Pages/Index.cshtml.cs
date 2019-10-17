f53560c6a3e841b78b618047dca45e0cf2a06a11
Jonathan Allen <jallen@snow.edu>
2019-10-17 10:25:35 -0600

    Create new razor pages website to work with Syncfusion diagram component

---
+++ ProgramPlanning.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ProgramPlanning.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        public void OnGet()
+        {
+            List<LocalDataDetails> localData = new List<LocalDataDetails>();
+            localData.Add(new LocalDataDetails("Species", ""));
+            localData.Add(new LocalDataDetails("Plants", "Species"));
+            localData.Add(new LocalDataDetails("Fungi", "Species"));
+            localData.Add(new LocalDataDetails("Lichens", "Species"));
+            localData.Add(new LocalDataDetails("Animals", "Species"));
+            localData.Add(new LocalDataDetails("Mosses", "Plants"));
+            localData.Add(new LocalDataDetails("Ferns", "Plants"));
+            localData.Add(new LocalDataDetails("Gymnosperms", "Plants"));
+            localData.Add(new LocalDataDetails("Dicotyledens", "Plants"));
+            localData.Add(new LocalDataDetails("Monocotyledens", "Plants"));
+            localData.Add(new LocalDataDetails("Invertebrates", "Animals"));
+            localData.Add(new LocalDataDetails("Vertebrates", "Animals"));
+            localData.Add(new LocalDataDetails("Insects", "Invertebrates"));
+            localData.Add(new LocalDataDetails("Molluscs", "Invertebrates"));
+            localData.Add(new LocalDataDetails("Crustaceans", "Invertebrates"));
+            localData.Add(new LocalDataDetails("Others", "Invertebrates"));
+            localData.Add(new LocalDataDetails("Fish", "Vertebrates"));
+            localData.Add(new LocalDataDetails("Amphibians", "Vertebrates"));
+            localData.Add(new LocalDataDetails("Reptiles", "Vertebrates"));
+            localData.Add(new LocalDataDetails("Birds", "Vertebrates"));
+            localData.Add(new LocalDataDetails("Mammals", "Vertebrates"));
+
+            ViewData["Nodes"] = localData;
+
+            ViewData["getNodeDefaults"] = "getNodeDefaults";
+            ViewData["getConnectorDefaults"] = "getConnectorDefaults";
+        }
+    }
+
+    public class LocalDataDetails
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+
+        public LocalDataDetails(string id, string category)
+        {
+            this.Name = id;
+            this.Category = category;
+        }
+    }
+}
---
+++ ProgramPlanning.Web/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ProgramPlanning.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTUwMTIzQDMxMzcyZTMzMmUzMElhUDNrSmV0MzVWRDFDMk5VYnB1QVNoRHlYOHF6dEk1OXJYcFNkUUFydG89;MTUwMTI0QDMxMzcyZTMzMmUzMEl0OWI3cVI1Sm11WlEyZDdGN3lyYmh6VlNaTndKN3BBTlZFUlhkSUVCTG89;MTUwMTI1QDMxMzcyZTMzMmUzME5BbjFGT3p2SlZmSStzb2JqRk9uMHNBSTNpMXdoaHVrMXlvc2lYbmhTYkE9;MTUwMTI2QDMxMzcyZTMzMmUzMElCNWhnQVFXOVNpb0h4aC8wa0NnVmdDM2l1N29xQU93SU9Tck5OdGcrcHM9;MTUwMTI3QDMxMzcyZTMzMmUzMFgrZDU1Nk5PQ052L0xRTHo1dkxqN1I3cFgwV2ZieXR1YlNMRUFDTzZTSmc9;MTUwMTI4QDMxMzcyZTMzMmUzMGs2TDdXQU56U05UWGI3bVQrWU5RRWxMYUtDZ296amN3d1VVOEpGTkQ1dTA9;MTUwMTI5QDMxMzcyZTMzMmUzMFpZUXhmbmpHdUlCalZuTnFTRml0VVlxU1JORE9xNmYyZytycnpjaXVndW89;MTUwMTMwQDMxMzcyZTMzMmUzMGgrMm14eGNYUVJCampTTFFLbXRCOWdvbDNzYWowTS9UemRMSDBGbWJzVG89;MTUwMTMxQDMxMzcyZTMzMmUzMGM3a1VrUy9nclZsa2dKdTY3MjQ0OTVTQlRDS0I1Ym0raGVTQ3ZPbmZ1VDg9;MTUwMTMyQDMxMzcyZTMzMmUzMFgrZDU1Nk5PQ052L0xRTHo1dkxqN1I3cFgwV2ZieXR1YlNMRUFDTzZTSmc9");
+        }
+    }
+}
