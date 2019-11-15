e279fb6146587cfaf77336d63d09f7fa58114760
Jonathan Allen <jallen@snow.edu>
2019-11-15 16:05:48 -0700

    Add stub for dapper integration w/postgres

---
+++ ProgramPlanning.Shared/Services/PostgresCourseRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Npgsql;
+using ProgramPlanning.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramPlanning.Shared.Services
+{
+    public class PostgresCourseRepository : ICourseInfoRepository
+    {
+        private readonly string connectionString;
+
+        public PostgresCourseRepository(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("message", nameof(connectionString));
+            }
+
+            this.connectionString = connectionString;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            var courses = new List<Course>();
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                var rows = conn.Query<QueryRow>("select [columns] from [tables] [where]", new { ParamName = "Bogus" });
+                foreach(var row in rows)
+                {
+                    //map row columns to course, learning outcome and skill
+                }
+            }
+
+            return courses;
+        }
+    }
+
+    public class QueryRow
+    {
+
+    }
+}
