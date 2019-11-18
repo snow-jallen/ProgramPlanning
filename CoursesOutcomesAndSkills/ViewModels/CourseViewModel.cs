fb525018415e723623ccde91c5bb3a3cace6f3c0
Jonathan Allen <jallen@snow.edu>
2019-11-18 10:35:16 -0700

    DataTemplate to highlight prepreq courses should work, but isn't... :-(

--- CoursesOutcomesAndSkills/ViewModels/CourseViewModel.cs
+++ CoursesOutcomesAndSkills/ViewModels/CourseViewModel.cs
@@ -9,31 +9,80 @@ namespace CoursesOutcomesAndSkills.ViewModels
 {
     public class CourseViewModel : ViewModelBase
     {
-        private readonly IEnumerable<Course> courses;
+        private readonly IEnumerable<IndividualCourseViewModel> courses;
 
         public CourseViewModel(IEnumerable<Course> courses)
         {
-            this.courses = courses ?? throw new ArgumentNullException(nameof(courses));
+            if (courses == null)
+                throw new ArgumentNullException(nameof(courses), "Must have a list of courses");
+            this.courses = courses.Select(c => new IndividualCourseViewModel(c));
         }
 
-        public IEnumerable<Course> PreReqCourses => courses.Where(c => c.Semester == Semester.PreReq);
-        public IEnumerable<Course> Year1Fall => courses.Where(c => c.Semester == Semester.Year1Fall);
-        public IEnumerable<Course> Year1Spring => courses.Where(c => c.Semester == Semester.Year1Spring);
-        public IEnumerable<Course> Year2Fall => courses.Where(c => c.Semester == Semester.Year2Fall);
-        public IEnumerable<Course> Year2Spring => courses.Where(c => c.Semester == Semester.Year2Spring);
-        public IEnumerable<Course> Year3Fall => courses.Where(c => c.Semester == Semester.Year3Fall);
-        public IEnumerable<Course> Year3Spring => courses.Where(c => c.Semester == Semester.Year3Spring);
-        public IEnumerable<Course> Year4Fall => courses.Where(c => c.Semester == Semester.Year4Fall);
-        public IEnumerable<Course> Year4Spring => courses.Where(c => c.Semester == Semester.Year4Spring);
-        private Course selectedCourse;
-        public Course SelectedCourse
+        public IEnumerable<IndividualCourseViewModel> PreReqCourses => courses.Where(c => c.Semester == Semester.PreReq);
+        public IEnumerable<IndividualCourseViewModel> Year1Fall => courses.Where(c => c.Semester == Semester.Year1Fall);
+        public IEnumerable<IndividualCourseViewModel> Year1Spring => courses.Where(c => c.Semester == Semester.Year1Spring);
+        public IEnumerable<IndividualCourseViewModel> Year2Fall => courses.Where(c => c.Semester == Semester.Year2Fall);
+        public IEnumerable<IndividualCourseViewModel> Year2Spring => courses.Where(c => c.Semester == Semester.Year2Spring);
+        public IEnumerable<IndividualCourseViewModel> Year3Fall => courses.Where(c => c.Semester == Semester.Year3Fall);
+        public IEnumerable<IndividualCourseViewModel> Year3Spring => courses.Where(c => c.Semester == Semester.Year3Spring);
+        public IEnumerable<IndividualCourseViewModel> Year4Fall => courses.Where(c => c.Semester == Semester.Year4Fall);
+        public IEnumerable<IndividualCourseViewModel> Year4Spring => courses.Where(c => c.Semester == Semester.Year4Spring);
+        private IndividualCourseViewModel selectedCourse;
+        public IndividualCourseViewModel SelectedCourse
         {
             get { return selectedCourse; }
             set
             {
+                clearPrerequisites(selectedCourse);
                 Set(ref selectedCourse, null);//un-select the prior one
+
                 Set(ref selectedCourse, value);//select the new one
+                highlightPrerequisites(selectedCourse);
             }
+        }
+
+        private void clearPrerequisites(IndividualCourseViewModel selectedCourse)
+        {
+            if (selectedCourse?.Course?.Prerequisites == null)
+                return;
+            foreach(var preReq in selectedCourse.Course.Prerequisites)
+            {
+                var courseVM = courses.Single(c => c.Course == preReq);
+                courseVM.IsPrerequisiteToCurrentCourse = false;
+            }
+        }
+
+        private void highlightPrerequisites(IndividualCourseViewModel selectedCourse)
+        {
+            if (selectedCourse?.Course?.Prerequisites == null)
+                return;
+            foreach (var preReq in selectedCourse.Course.Prerequisites)
+            {
+                var courseVM = courses.Single(c => c.Course == preReq);
+                courseVM.IsPrerequisiteToCurrentCourse = true;
+            }
+        }
+    }
+
+    public class IndividualCourseViewModel : ViewModelBase
+    {
+        public IndividualCourseViewModel(Course course)
+        {
+            Course = course ?? throw new ArgumentNullException(nameof(course));
+        }
+
+        public Course Course { get; }
+        public string CourseNumber => Course.CourseNumber;
+        public string Title => Course.Title;
+        public Semester Semester => Course.Semester;
+        public string Summary => Course.Summary;
+        public IEnumerable<LearningOutcome> Outcomes => Course.Outcomes;
+
+        private bool isPrerequisiteToCurrentCourse;
+        public bool IsPrerequisiteToCurrentCourse
+        {
+            get => isPrerequisiteToCurrentCourse;
+            set { Set(ref isPrerequisiteToCurrentCourse, value); }
         }
 
     }
--- ProgramPlanning.Shared/Services/BogusCourseInfoRepository.cs
+++ ProgramPlanning.Shared/Services/BogusCourseInfoRepository.cs
@@ -29,7 +29,7 @@ namespace ProgramPlanning.Shared.Services
 
             var cs1400 = new Course("CS", 1400, Semester.PreReq, "Intro to Programming - Lecture", prerequisites: null, outcomes: new[] { cs1400_lo1, cs1400_lo2, cs1400_lo3 });
             var cs1405 = new Course("CS", 1405, Semester.PreReq, "Intro to Programming - Lab");
-            var cs1410 = new Course("CS", 1410, Semester.Year1Fall, "OO Programming - Lecture");
+            var cs1410 = new Course("CS", 1410, Semester.Year1Fall, "OO Programming - Lecture", prerequisites: new[] { cs1400, cs1405 });
             var cs1415 = new Course("CS", 1415, Semester.Year1Fall, "00 Programming - Lab");
             var cs2700 = new Course("CS", 2700, Semester.Year1Spring, "Digital Circuits", summary: "This class is great because it has such an awesome summary.  The summary though, really, is definitely what makes this course.  In this course we review the summary, and other summarized articles.  In summary, the summary of this course is summarily great.  That about sums it up.");
             var cs1420 = new Course("CS", 1420, Semester.Year1Spring, "Craziness");
